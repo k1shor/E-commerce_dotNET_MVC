336e655f55dd467fea13bc233ba00c063518bbec
k1shor <k1shor@hotmail.com>
2023-07-11 11:48:28 +0545

    identity scaffold added

--- E-commerce.Data/ApplicationDbContext.cs
+++ E-commerce.Data/ApplicationDbContext.cs
@@ -1,10 +1,11 @@
 ﻿using E_commerce.Models;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace E_commerce.Data
 {
-    public class ApplicationDbContext : IdentityDbContext
+    public class ApplicationDbContext : IdentityDbContext<IdentityUser>
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
         {
