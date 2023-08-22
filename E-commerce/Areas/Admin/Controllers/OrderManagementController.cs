262cd6f167d3769701aa2f2771b1c55a5974e9b4
k1shor <k1shor@hotmail.com>
2023-08-22 16:26:01 +0545

    Update order details

--- E-commerce/Areas/Admin/Controllers/OrderManagementController.cs
+++ E-commerce/Areas/Admin/Controllers/OrderManagementController.cs
@@ -2,6 +2,7 @@
 using E_Commerce.Models;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
 using Utils;
 
 namespace E_commerce.Areas.Admin.Controllers
@@ -18,6 +19,7 @@ namespace E_commerce.Areas.Admin.Controllers
         {
             return View();
         }
+
         public IActionResult OrderDetails(int orderId)
         {
             OrderVM orderObj = new()
@@ -27,14 +29,42 @@ namespace E_commerce.Areas.Admin.Controllers
             };
             return View(orderObj);
         }
+        [HttpPost]
+        [Authorize]
+        public IActionResult UpdateOrder(OrderVM orderObj)
+        {
+            var orderHeader = _unitOfWork.OrderHeader.FirstOrDefault(u => u.Id == orderObj.OrderHeader.Id);
+            orderHeader.Name = orderObj.OrderHeader.Name;
+            orderHeader.PhoneNumber = orderObj.OrderHeader.PhoneNumber;
+            orderHeader.StreetAddress = orderObj.OrderHeader.StreetAddress;
+            orderHeader.City = orderObj.OrderHeader.City;
+            orderHeader.State = orderObj.OrderHeader.State;
+            orderHeader.PostalCode = orderObj.OrderHeader.PostalCode;
+            _unitOfWork.OrderHeader.Update(orderHeader);
+            _unitOfWork.Save();
+            TempData["success"] = "Order Details Updated Successfully.";
+            return RedirectToAction(nameof(OrderDetails), new { orderId = orderHeader.Id});
+        }
 
 
         #region API CALLS
 
         [HttpGet]
+        [Authorize]
         public IActionResult GetAll(string status)
         {
-            List<OrderHeader> objOrderHeaders = _unitOfWork.OrderHeader.FindAll(includeProperties: "ApplicationUser").ToList();
+            List<OrderHeader> objOrderHeaders;
+            if (User.IsInRole(StaticData.ROLE_ADMIN))
+            {
+                objOrderHeaders = _unitOfWork.OrderHeader.FindAll(includeProperties: "ApplicationUser").ToList();
+            }
+            else
+            {
+                var claimsIdentity = (ClaimsIdentity)User.Identity;
+                var userId = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier).Value;
+                objOrderHeaders = _unitOfWork.OrderHeader.FindAll(u => u.ApplicationUserId == userId, includeProperties: "ApplicationUser").ToList();
+
+            }
             switch (status)
             {
                 case "PENDING":
